e501abaf81447bf1f02447116a31d1812840fa80
cloudsecdee@gmail.com <19dishen@gmail.com>
2025-02-17 22:02:09 +0000

    Updated Terraform EKS configuration

---
+++ .history/terraform/eks-cluster_20250217215904.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attach necessary IAM policies to the EKS Cluster Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "flask-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.eks_subnet[*].id
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+
+  instance_types = ["t3.medium"]
+
+  remote_access {
+    ec2_ssh_key = var.gitopskey
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_nodes,
+    aws_iam_role_policy_attachment.eks_worker_nodes_cni
+  ]
+}
